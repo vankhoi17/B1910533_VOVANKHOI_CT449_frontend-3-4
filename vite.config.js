342a197f4387e7787e2a40321317b84393b45a46
vovankhoi <khoib1910533@student.ctu.edu.vn>
2023-10-27 09:07:05 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   server: {
     port: 3001,
+    proxy: {
+      "/api": {
+      target: "http://localhost:3000/",
+      changeOrigin: true,
+      },
+      }      
     },    
   plugins: [
     vue(),
